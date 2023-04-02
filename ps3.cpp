f5275d802f61e52688f7f1e5eda00116625bddfc
Mohamed Hassan <87271791+Mh291093@users.noreply.github.com>
2023-04-02 21:23:34 +0200

    Add files via upload

---
+++ ps3.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main () {
+	long long x, y;
+	
+	cin >> x >> y;
+	
+	cout << x << " + " << y << " = " << x + y << endl;
+	cout << x << " * " << y << " = " << x * y << endl;
+	cout << x << " - " << y << " = " << x - y << endl;
+}
