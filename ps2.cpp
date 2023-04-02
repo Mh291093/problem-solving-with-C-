c0869654848eefaf5185855f40a2b7e078e95db1
Mohamed Hassan <87271791+Mh291093@users.noreply.github.com>
2023-04-02 21:20:22 +0200

    Add files via upload

---
+++ ps2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main () {
+    int x;
+	long long y;
+	char a;
+	float z;
+	double u;
+	
+    cin >> x >> y >> a >> z >> u;
+    cout << x << "\n" << y << "\n" << a << "\n" << z << "\n" << u;
+}
+
