4a6b7c4a38331e9616c199097a9abb96b819793e
Mohamed Hassan <87271791+Mh291093@users.noreply.github.com>
2023-04-02 21:28:10 +0200

    Update ps5.cpp

--- ps5.cpp
+++ ps5.cpp
@@ -1,3 +1,13 @@
+/*
+Given a number R calculate the area of a circle using the following formula:
+Area = π * R2.
+Note: consider π = 3.141592653.
+Input
+Only one line containing the number R (1  ≤  R  ≤  100).
+
+Output
+Print the calculated area, with 9 digits after the decimal point.
+*/
 #include<iostream>
 #include<iomanip>
 using namespace std;
