a3d41659e603a0f76476a5835091eb565b2877ce
Mohamed Hassan <87271791+Mh291093@users.noreply.github.com>
2023-04-02 21:26:43 +0200

    Update ps4.cpp

--- ps4.cpp
+++ ps4.cpp
@@ -1,3 +1,12 @@
+/*
+Given four numbers A, B, C and D. Print the result of the following equation :
+ X = (A * B) - (C * D).
+Input
+Only one line containing 4 separated numbers A, B, C and D ( - 105  ≤  A, B, C, D  ≤  105).
+
+Output
+Print "Difference  =  " without quotes followed by the equation result.
+*/
 #include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
