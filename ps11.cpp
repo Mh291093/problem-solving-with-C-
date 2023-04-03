e11eb569836906f33cf47b92954a463d50922e5f
Mohamed Hassan <87271791+Mh291093@users.noreply.github.com>
2023-04-03 20:51:35 +0200

    Add files via upload

---
+++ ps11.cpp
@@ -0,0 +1,29 @@
+// Given 3 numbers A, B and C, Print the minimum and the maximum numbers
+
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main () {
+	int num1, num2, num3;
+	cin >> num1 >> num2 >> num3;
+	if (num1 >= num2 && num1 >= num3) {
+		if (num2 > num3) {
+			cout << num3 << " " << num1 << endl;
+		} else {
+			cout << num2 << " " << num1 << endl;
+		}
+	} else if (num2 >= num1 && num2 >= num3) {
+		if (num1 > num3) {
+			cout << num3 << " " << num2 << endl;
+		} else {
+			cout << num1 << " " << num2 << endl;
+		}
+	} else if (num3 >= num1 && num3 >= num2) {
+		if (num1 > num2) {
+			cout << num2 << " " << num3 << endl;
+		} else {
+			cout << num1 << " " << num3 << endl;
+		}
+	}
+}
