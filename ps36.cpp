cded2ddf4b4de14911fe7acdaa0faa07c111a6a6
Mohamed Hassan <87271791+Mh291093@users.noreply.github.com>
2023-04-20 15:53:33 +0200

    Add files via upload

---
+++ ps36.cpp
@@ -0,0 +1,12 @@
+/* Given a number N. Print numbers from 1 to N in separate lines.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+	int num;
+	cin >> num;
+	for (int i = 1; i <= num; i++) {
+		cout << i << endl;
+	}
+}
